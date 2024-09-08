8a02dafb87645d600f75684e9b716ce172880274
kyo <odinxp@gmail.com>
2024-09-08 17:29:05 +0800

    feat: implement new NinjaAPI for user and post routes
    - Introduce a new API using NinjaAPI in a new file
    - Add routes for users and posts in the new API
    - Include the new API in the main URL configuration
    
    Signed-off-by: kyo <odinxp@gmail.com>

--- NinjaForum/urls.py
+++ NinjaForum/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from NinjaForum.api import api
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', api.urls),
 ]
