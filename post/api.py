2b7b85c84c2ffab8ddd12bf99f9637af9f4474e9
kyo <odinxp@gmail.com>
2024-09-12 21:51:48 +0800

    feat: improve post handling and documentation in API functions
    - Add docstrings to the `get_posts`, `get_post`, and `create_post` functions for better documentation and understanding.
    - Enhance the `get_posts` function to include a new query parameter for filtering posts by title.
    - Improve the `create_post` function to handle post creation with provided payload data.
    
    Signed-off-by: kyo <odinxp@gmail.com>

--- post/api.py
+++ post/api.py
@@ -13,6 +13,9 @@ def get_posts(
     request: HttpRequest,
     title: None | str = Query(None, min_length=2, max_length=10),
 ) -> QuerySet[Post]:
+    """
+    取得文章列表
+    """
     posts = Post.objects.all()
     if title:
         posts = posts.filter(title__icontains=title)
@@ -21,12 +24,18 @@ def get_posts(
 
 @router.get(path='/posts/{int:post_id}/')
 def get_post(request: HttpRequest, post_id: int) -> Post:
+    """
+    取得單一文章
+    """
     post = Post.objects.get(id=post_id)
     return post
 
 
 @router.post(path='/posts/')
 def create_post(request: HttpRequest, payload: CreatePostRequest) -> tuple[int, dict]:
+    """
+    新增文章
+    """
     post = Post.objects.create(
         title=payload.title,
         content=payload.content,
