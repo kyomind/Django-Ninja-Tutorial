fddfd26a913e1e202956337a4949b1b6acea14e5
kyo <odinxp@gmail.com>
2024-09-18 18:07:12 +0800

    feat: enhance API configuration with tag support
    - Add tags to the user and post routers in the API configuration.
    
    Signed-off-by: kyo <odinxp@gmail.com>

--- NinjaForum/api.py
+++ NinjaForum/api.py
@@ -2,5 +2,5 @@ from ninja import NinjaAPI
 
 api = NinjaAPI()
 
-api.add_router(prefix='', router='user.api.router')
-api.add_router(prefix='', router='post.api.router')
+api.add_router(prefix='', router='user.api.router', tags=['User'])
+api.add_router(prefix='', router='post.api.router', tags=['Post'])
